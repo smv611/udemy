39ee89b76736bb29f1bfa8b4fe9f34750ea05297
smv611@mail.ru <smv611@mail.ru>
2019-11-07 12:43:01 +0300

    second commit

--- runtest.js
+++ runtest.js
@@ -11,11 +11,8 @@ const http = require('http');
 const server = http.createServer((req, res)=>{
     console.log(req.url, req.method, req.headers);
     //process.exit();;
-    //res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Type', 'text/html');
     res.write('<h1>hello world!</h1>');
-    res.write('<h1>hello world!</h1>');
-    res.write('<h1>hello world!</h1>');
-
 });
 server.listen(3000);
 
