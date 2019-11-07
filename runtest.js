a8633a338ed4006f7946e8e53bf7fa90f423c98e
smv611 <smv611@mail.ru>
2019-11-07 12:29:30 +0300

    first commit

---
+++ runtest.js
@@ -0,0 +1,22 @@
+// simple example
+// console.log('hello fron nodejs!')
+
+// file system
+    // let fs = require('fs');
+    // fs.writeFileSync('test-File2.txt', 'xxxxxxxxxxxx');    
+
+
+// http server
+const http = require('http');
+const server = http.createServer((req, res)=>{
+    console.log(req.url, req.method, req.headers);
+    //process.exit();;
+    //res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>hello world!</h1>');
+    res.write('<h1>hello world!</h1>');
+    res.write('<h1>hello world!</h1>');
+
+});
+server.listen(3000);
+
+
